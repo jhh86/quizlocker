a20972d84d47003dbee0ca130f1c3dc4a44c029f
jhh86 <rparhoon@nate.com>
2020-07-03 17:32:55 +0900

    20200703_07.10

--- app/src/main/java/com/example/quizlocker/BootCompleteReceiver.kt
+++ app/src/main/java/com/example/quizlocker/BootCompleteReceiver.kt
@@ -6,15 +6,15 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.preference.PreferenceManager
-
 import android.util.Log
 import com.example.quizlocker.LockScreenService
 
+// BroadcastReceiver 를 상속받는다
 class BootCompleteReceiver : BroadcastReceiver() {
-
+    // 브로드캐스트 메세지 수신시 불리는 컬백 함수
     override fun onReceive(context: Context?, intent: Intent?) {
         when {
-
+            // 부팅이 완료될때의 메세지인지 확인
             intent?.action == Intent.ACTION_BOOT_COMPLETED -> {
                 Log.d("quizlocker", "부팅이 완료됨")
                 context?.let {
@@ -22,6 +22,7 @@ class BootCompleteReceiver : BroadcastReceiver() {
                     val pref = PreferenceManager.getDefaultSharedPreferences(context)
                     val useLockScreen = pref.getBoolean("useLockScreen", false)
                     if (useLockScreen) {
+                        // LockScreenService 시작
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                             it.startForegroundService(Intent(context, LockScreenService::class.java))
                         } else {
